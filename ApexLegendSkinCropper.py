9adba867343edfd7a7069a85615d72475b4e276b
samcarroll42 <sjc7622@rit.edu>
2023-02-14 17:48:49 -0500

    Added pseudocode

--- ApexLegendSkinCropper.py
+++ ApexLegendSkinCropper.py
@@ -1,2 +1,17 @@
 import os
 import PIL
+
+# Take user input for folder path
+
+# Go to folder path
+
+# Iterate over files
+    # For file in folder:
+        # Check which Legend skin belongs to (ex. endswith(Bangalore.png))
+
+        # Use Pillow to crop image to specified coordinates
+
+        # If file is for weapon / charm / not applicable:
+            # Skip/continue
+
+    
